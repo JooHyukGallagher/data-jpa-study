9b7728f7cf7adb32aadf34688f49c1adf594731b
joohyuk <vfrvfr4207@gmail.com>
2020-03-04 20:04:18 +0900

    Create findByAge() with Pageable

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,10 +1,15 @@
 package study.datajpa.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import study.datajpa.entity.Member;
+
 
 import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+    Page<Member> findByAge(int age, Pageable pageable);
 }
