5aa6f4d4a8085423ab97a74ccbb249f433731824
joohyuk <vfrvfr4207@gmail.com>
2020-03-05 17:00:00 +0900

    Create bulkAgePlus() & Test

--- src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -45,15 +45,26 @@ public class MemberJpaRepository {
     public List<Member> findByPage(int age, int offset, int limit) {
         return em.createQuery("select m from Member m where m.age = :age order by " +
                 "m.username desc")
-                        .setParameter("age", age)
-                        .setFirstResult(offset)
-                        .setMaxResults(limit)
-                        .getResultList();
+                .setParameter("age", age)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
     }
+
     public long totalCount(int age) {
         return em.createQuery("select count(m) from Member m where m.age = :age",
                 Long.class)
                 .setParameter("age", age)
                 .getSingleResult();
+    }
+
+    public int bulkAgePlus(int age) {
+        int resultCount = em.createQuery(
+                "UPDATE Member m set m.age = m.age + 1" +
+                        "WHERE m.age >= :age")
+                .setParameter("age", age)
+                .executeUpdate();
+
+        return resultCount;
     }
 }
--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -3,6 +3,9 @@ package study.datajpa.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import study.datajpa.entity.Member;
 
 
@@ -12,4 +15,8 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
 
     Page<Member> findByAge(int age, Pageable pageable);
+
+    @Modifying
+    @Query("UPDATE Member m SET m.age = m.age + 1 WHERE m.age >= :age")
+    int bulkAgePlus(@Param("age") int age);
 }
